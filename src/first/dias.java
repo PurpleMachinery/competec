820636270942393b974b8c72a5fe775b91102cb1
Wesley V. Lima <wesley.vieira.lima.1999@gmail.com>
2017-08-06 21:46:44 -0300

    Dias Pronto

---
+++ src/first/dias.java
@@ -0,0 +1,14 @@
+package first;
+import java.util.Scanner;
+public class dias {
+	public static void main(String args[]) {
+		Scanner entrada=new Scanner(System.in);
+		int i=entrada.nextInt();
+		while(i!=0) {
+			i*=365;
+			System.out.println(i);
+			
+			i=entrada.nextInt();
+		}
+	}
+}

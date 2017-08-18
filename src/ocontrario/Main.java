4e18e8056efd6688e3d0ecfe2a4c46a4155a81ac
Wesley vieira de lima <wesley.vieira.lima.1999@gmail.com>
2017-08-18 19:36:02 -0300

    Comentando o código.

--- src/ocontrario/Main.java
+++ src/ocontrario/Main.java
@@ -7,13 +7,13 @@ public class Main {
 		Scanner entrada = new Scanner(System.in);
 
 		do {
-			Metodo reverter = new Metodo();
+			Metodo reverter = new Metodo(); //Criação do objeto reverter.
 			String texto = entrada.nextLine();
 			if (texto.length() < 2)
 				return;
 			for (int i = texto.length() - 1; i >= 0; i--) {
-				reverter.SetTexto(texto, i);
-				System.out.print(reverter.GetTexto());
+				reverter.SetTexto(texto, i); //Mandando a String e o numero da contagem para o método.
+				System.out.print(reverter.GetTexto()); //Pega o Character para reverter a string até que vá para posição 0.
 			}
 			System.out.print("\n");
 		} while (true != false);
